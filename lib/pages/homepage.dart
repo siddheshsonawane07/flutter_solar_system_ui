b7129a6c83b4e8480e8b78a8ff0151d0d52a09e1
Siddhesh Sonawane <siddheshsonawane07@gmail.com>
2022-05-24 22:26:22 +0530

    assets added

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -10,8 +10,33 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Container(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Text(
+                "Explore",
+                style: TextStyle(
+                    fontFamily: "Avenir",
+                    fontSize: 44,
+                    color: const Color(0xffffffff),
+                    fontWeight: FontWeight.w900),
+                textAlign: TextAlign.left,
+              ),
+              DropdownButton(
+                items: DropdownMenuItem(child: Text('Solar System')),
+                onChanged: (value) {},
+                icon: Padding(
+                  padding: const EdgeInsets.only(left: 16.0),
+                  child: Image.asset('assets/dropdown.png'),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
